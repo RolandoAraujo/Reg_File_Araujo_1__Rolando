15b7d5f5bae5ea532cd766b32f9da68d7a973ece
RolandoAraujo <89765777+RolandoAraujo@users.noreply.github.com>
2021-11-26 00:07:40 -0700

    Fixed decoder code

--- src/decoder.v
+++ src/decoder.v
@@ -1,42 +1,95 @@
-module decoder(sel,out);
+module decoder(
 
-input [4:0] sel;
-output reg [31:0] out;
+input [4:0] a,                                 
 
-always @(sel) begin 
-case(sel)
-5'b00000: out[0] = 1'b1;
-5'b00001: out[1] = 1'b1;
-5'b00010: out[2] = 1'b1;
-5'b00011: out[3] = 1'b1;
-5'b00100: out[4] = 1'b1;
-5'b00101: out[5] = 1'b1;
-5'b00110: out[6] = 1'b1;
-5'b00111: out[7] = 1'b1;
-5'b01000: out[8] = 1'b1;
-5'b01001: out[9] = 1'b1;
-5'b01010: out[10] = 1'b1;
-5'b01011: out[11] = 1'b1;
-5'b01100: out[12] = 1'b1;
-5'b01101: out[13] = 1'b1;
-5'b01110: out[14] = 1'b1;
-5'b01111: out[15] = 1'b1;
-5'b10000: out[16] = 1'b1;
-5'b10001: out[17] = 1'b1;
-5'b10010: out[18] = 1'b1;
-5'b10011: out[19] = 1'b1;
-5'b10100: out[20] = 1'b1;
-5'b10101: out[21] = 1'b1;
-5'b10110: out[22] = 1'b1;
-5'b10111: out[23] = 1'b1;
-5'b11000: out[24] = 1'b1;
-5'b11001: out[25] = 1'b1;
-5'b11010: out[26] = 1'b1;
-5'b11011: out[27] = 1'b1;
-5'b11100: out[28] = 1'b1;
-5'b11101: out[29] = 1'b1;
-5'b11110: out[30] = 1'b1;
-5'b11111: out[31] = 1'b1;
-endcase 
-end 
+input en,                                      
+ 
+output [31:0] enc );                       
+
+wire [3:0] w;                                
+
+decoder2x4 x1(w,a[4:3],en);                                                             
+
+decoder3x8 x2(enc[7:0], a[2:0],w[0]);  
+
+decoder3x8 x3(enc[15:8], a[2:0],w[1]);
+
+decoder3x8 x4(enc[23:16], a[2:0],w[2]);
+
+decoder3x8 x5(enc[31:24], a[2:0],w[3]);
+
+endmodule
+
+
+
+module decoder2x4(
+
+output reg [3:0] enc,
+
+input [1:0] a,
+
+input en);
+
+always @(a,en)               
+
+
+if(en)
+
+case(a)
+
+2'b00: enc=4'b0001;    
+
+2'b01: enc=4'b0010;
+
+2'b10: enc=4'b0100;
+
+2'b11: enc=4'b1000;
+
+default: enc=0;
+
+endcase
+
+else enc=0;              
+
+endmodule
+
+
+
+module decoder3x8(
+
+output reg [7:0] enc,
+
+input [2:0] a,
+
+input en);
+
+always @(a,en)
+
+if(en)
+
+case(a)                                  
+
+
+0: enc =8'b00000001;           
+
+1: enc =8'b00000010;           
+
+2: enc =8'b00000100;
+
+3: enc =8'b00001000;
+
+4: enc =8'b00010000;
+
+5: enc =8'b00100000;
+
+6: enc =8'b01000000;
+
+7: enc =8'b10000000;
+
+default: enc=0;
+
+endcase
+
+else enc=0;        
+
 endmodule
